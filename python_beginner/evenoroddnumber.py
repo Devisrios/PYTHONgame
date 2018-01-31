77e8ab2cc84f4ae7f3ddcab8339b5d5404af232c
Devisrios <33158227+Devisrios@users.noreply.github.com>
2018-01-31 10:08:14 -0800

    Create evenoroddnumber.py

---
+++ python_beginner/evenoroddnumber.py
@@ -0,0 +1,12 @@
+def main():
+
+      num=int(input("Enter the number"))
+      if (num%2==0):
+         print("Even number")
+      else:
+        print("Odd number")
+
+
+
+if __name__ == '__main__':
+    main()
