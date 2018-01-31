b48f9105bbb27b3cae94e9be1014e28076d08c06
Devisrios <33158227+Devisrios@users.noreply.github.com>
2018-01-31 10:24:01 -0800

    Create vowelorconsonant.py

---
+++ python_beginner/vowelorconsonant.py
@@ -0,0 +1,16 @@
+def main():
+
+      list=['a','e','i','o','u']
+      list1=['A','E','I','O','u']
+      x=input("Enter the number")
+      if(x in list  or x in list1):
+        print("vowel")
+      else:
+        print("consonant")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
