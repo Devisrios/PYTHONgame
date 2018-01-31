b3e5f73b918d362d814c78790c87345f47da30b3
Devisrios <33158227+Devisrios@users.noreply.github.com>
2018-01-31 09:47:15 -0800

    Create posorneg.py

---
+++ python_beginner/posorneg.py
@@ -0,0 +1,11 @@
+def main():
+    num=int(input("Enter the number"))
+    if(num>0):
+        print("Positive number")
+    else:
+        print("Negative number")
+
+
+
+if __name__ == '__main__':
+    main()
